67956533fb0a5d3b766bca09369817a31f62b59f
SYUICore <ui-core@sydle.com>
2025-10-30 13:10:43 -0300

    Updated button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -11,7 +11,7 @@ const buttonVariants = cva(
         primary:
           "bg-[#161616] text-[#FFFAF3] border-2 border-[#F99830] shadow-[2px_2px_0px_#F99830] hover:shadow-[3px_3px_0px_#F99830] hover:translate-x-[-1px] hover:translate-y-[-1px] active:shadow-[1px_1px_0px_#F99830] active:translate-x-[1px] active:translate-y-[1px]",
         secondary:
-          "bg-[#F9A858] text-[#161616] border-2 border-[#161616] shadow-none hover:bg-[#FF9D42] hover:translate-y-[-2px] hover:shadow-[0_4px_12px_rgba(0,0,0,0.15)] active:translate-y-[0px] active:shadow-none",
+          "bg-[#F9A858] text-[#161616] border-2 border-[#161616] shadow-[2px_2px_0px_#161616] hover:shadow-[3px_3px_0px_#161616] hover:translate-x-[-1px] hover:translate-y-[-1px] active:shadow-[1px_1px_0px_#161616] active:translate-x-[1px] active:translate-y-[1px]",
         outline:
           "bg-transparent text-[#161616] border-2 border-[#161616] shadow-none hover:bg-[#161616] hover:text-[#FFFAF3] hover:shadow-[2px_2px_0px_#F99830] active:shadow-[1px_1px_0px_#F99830]",
         ghost:
